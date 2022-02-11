8088efe4c92c6b4a465c042b6aab8e842a9b6805
FrankVela <franciscojaviergomezvela@iessoterohernandez.com>
2022-02-11 09:19:28 +0100

    Subo Ej4yy

---
+++ Ej5/Tarjeta/src/test/java/dominio/CreditoTest.java
@@ -0,0 +1,134 @@
+package dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CreditoTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testRetirar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testIngresar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testPagoEnEstablecimiento() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetSaldo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testCredito() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetCreditoDisponible() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testLiquidar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testTarjeta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetCuenta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ Ej5/Tarjeta/src/test/java/dominio/CuentaTest.java
@@ -0,0 +1,124 @@
+package dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testCuenta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testIngresarDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRetirarDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testIngresarStringDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRetirarStringDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetSaldo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testAddMovimiento() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
